96135e38c8494454c34a8874680f1af8682958c7
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-03-16 15:40:14 -0500

    new files addition

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -32,7 +32,7 @@ const BodyComponent = () => {
 
   async function getRestaurants() {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json);
