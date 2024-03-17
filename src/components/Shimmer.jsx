27817d266ad7a007fe8b5f29220b69860a09bd1e
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-03-17 15:06:38 -0500

    packages

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,9 +1,15 @@
 const Shimmer = () => {
-    return (
-        <>
-        <h2>Loading Bro Wait....!</h2>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="food-card">
+        {Array(10)
+          .fill(" ")
+          .map((e, index) => (
+            <div key={index} className="shimmer"></div>
+          ))}
+      </div>
+    </>
+  );
+};
 
 export default Shimmer;
