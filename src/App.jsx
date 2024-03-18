33b31dcfc409f2cec87bb6082886d1432378e92b
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-03-18 10:38:54 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,68 @@ import ReactDOM from "react-dom/client";
 import HeaderComponent from "./components/Header"; //We can do this too instead of the below two import structures.
 import BodyComponent from "./components/Body";
 import FooterComponent from "./components/Footer";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
+import Contact from "./components/Contact";
+import RestaurantDetails from "./components/RestaurantDetail";
 
 const WebsiteLayout = () => {
   return (
     // <React.Fragment>
     <>
       <HeaderComponent />
-      <BodyComponent />
+      <Outlet />
       <FooterComponent />
     </>
     // </React.Fragment>
   );
 };
 
+/* <About />
+<BodyComponent />
+<Contact /> */
+
+// const appRoutes = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <WebsiteLayout />,
+//     errorElement: <Error />,
+//   },
+//   {
+//     path: "/about",
+//     element: <About />,
+//   },
+// ]);
+
 const appRoutes = createBrowserRouter([
   {
     path: "/",
     element: <WebsiteLayout />,
     errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
+    children: [
+      {
+        path: "/",
+        element: <BodyComponent />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurant/:id",
+        element: <RestaurantDetails />,
+      }
+    ],
   },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<RouterProvider router={appRoutes} />);
+
+//All the children in the routes goes into the outlet.
