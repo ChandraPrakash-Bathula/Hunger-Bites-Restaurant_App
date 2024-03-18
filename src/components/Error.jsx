0b2230fe686e0b3f2bb74d678c3d7eaebd751696
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-03-18 10:39:16 -0500

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,13 +1,14 @@
+import React from "react";
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
   const err = useRouteError();
-  const {status, statusText} = err;
+//   const {status, statusText} = err;
   return (
     <div>
       <h1>Oops!!</h1>
       <h2>Something Went Wrong</h2>
-      <h3>{status + " : " + statusText}</h3>
+      <h3>{err.status + " : " + err.statusText}</h3>
     </div>
   );
 };
