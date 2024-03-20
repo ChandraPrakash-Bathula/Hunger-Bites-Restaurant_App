b8997f0b5bd011fad9f833ff0e9cfd124a71ddea
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-03-20 12:21:44 -0500

    Update config.jsx

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,6 +1,7 @@
 export const IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
-
+  
+  export const MENU_API= "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.406498&lng=78.47724389999999&restaurantId="
 // This is known as Config Driven UI.
 export const restaurantList = [
   {
