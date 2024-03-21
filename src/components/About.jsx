42f40cc5142bd805aabb59e8e54c8927bb594b9f
Chandra Prakash Bathula <chandu.bathula16@gmail.com>
2024-03-21 11:02:48 -0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,15 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Profile from "./ProfileClass";
+import ProfileFunctionalComponent from "./Profile"
+
+
+
 const About = () => {
   return (
     <div>
       <h1>About Page</h1>
       <p>Restaurant details page</p>
-      <div className="food-card">
-        {Array(10)
-          .fill(" ")
-          .map((e, index) => (
-            <div key={index} className="shimmer"></div>
-          ))}
-      </div>
+      {/* <Outlet /> */}
+      <Profile name={"ClassComponentName"} />
+      <ProfileFunctionalComponent name={"Chandu"} />
     </div>
   );
 };
