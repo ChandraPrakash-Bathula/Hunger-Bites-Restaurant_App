59471080033ec897e71a96164f0e733e09b30408
ChandraPrakash-Bathula <chandu.bathula16@gmail>
2024-03-28 15:07:37 -0500

    test cases through role

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,14 @@
 const Contact = () => {
   return (
     <div>
-      <h1>This is a contact page bro.</h1>
+      <h1 className="font-bold text-3xl text-center m-4 p-4">
+        This is a contact page bro.
+      </h1>
+      <form>
+        <input type="text" className="border border-black m-2 p-2 rounded-sm" placeholder="name" />
+        <input type="text" className="border border-black m-2 p-2 rounded-sm" placeholder="message" />
+        <button className="border border-sky-800 m-2 p-2 rounded-lg hover:text-white hover:bg-sky-800" >Submit</button>
+      </form>
     </div>
   );
 };
